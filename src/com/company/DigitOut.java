d20bd837ce509d80edc541d36155258d8211f520
Rost87 <34788968+Rost87@users.noreply.github.com>
2021-01-29 17:30:27 +0500

    Добавлен вывод на экран чисел от 1 до 10 (ветка master)

--- src/com/company/DigitOut.java
+++ src/com/company/DigitOut.java
@@ -4,5 +4,8 @@ public class DigitOut {
     public static void main(String[] args) {
 
         System.out.println("Вывод чисел 1...10 на экран: ");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
     }
 }
