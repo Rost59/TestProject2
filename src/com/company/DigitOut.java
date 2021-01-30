0495c455c7699cb7932ab066882f30499bb8825c
Rost59 <78016305+Rost59@users.noreply.github.com>
2021-01-30 22:31:40 +0500

    Изменил алгоритм вывода чисел 1...10 (ветка master)

--- src/com/company/DigitOut.java
+++ src/com/company/DigitOut.java
@@ -4,8 +4,11 @@ public class DigitOut {
     public static void main(String[] args) {
 
         System.out.println("Вывод чисел 1...10 на экран: ");
-        for (int i = 1; i < 11; i++) {
+        // изменяю алгоритм вывода на экран числе 1...10
+        int i = 1;
+        while(i < 11){
             System.out.println(i);
+            i++;
         }
     }
 }
